ffb952689d68c795268e93efaeae474e6119cb08
Emiliano Qaqi <emilianoqaqi@msn.com>
2016-07-09 11:15:54 -0400

    Changed Urls

--- wsdear_scraper.rb
+++ wsdear_scraper.rb
@@ -2,9 +2,29 @@ require './category'
 require './exporter'
 
 urls = [
-    'http://www.wsdear.com/womens-clothing-sweats.html',
-    'http://www.wsdear.com/womens-jumpsuit.html',
-    'http://www.wsdear.com/cheap-jewelry.html',
+  'http://www.wsdear.com/womens-summer-dresses.html',
+  'http://www.wsdear.com/womens-black-dresses.html',
+  'http://www.wsdear.com/womens-chiffon-dresses.html',
+  'http://www.wsdear.com/womens-casual-dresses.html',
+  'http://www.wsdear.com/womens-lace-dresses.html',
+  'http://www.wsdear.com/womens-maxi-dresses.html',
+  'http://www.wsdear.com/womens-mini-dresses.html',
+  'http://www.wsdear.com/womens-party-club-dresses.html',
+  'http://www.wsdear.com/womens-bodycon-dresses.html',
+  'http://www.wsdear.com/womens-clothing-shirts-blouses.html',
+  'http://www.wsdear.com/womens-clothing-t-shirts.html',
+  'http://www.wsdear.com/womens-tank-tops.html',
+  'http://www.wsdear.com/womens-clothing-skirts.html',
+  'http://www.wsdear.com/womens-clothing-jeans.html',
+  'http://www.wsdear.com/womens-shorts-pants.html',
+  'http://www.wsdear.com/womens-trousers-pants.html',
+  'http://www.wsdear.com/womens-jumpsuit.html',
+  'http://www.wsdear.com/womens-clothing-sweats.html',
+  'http://www.wsdear.com/leggings.html',
+  'http://www.wsdear.com/womens-swimwear-beachwear.html',
+  'http://www.wsdear.com/womens-clothing-sweaters-cardigans.html',
+  'http://www.wsdear.com/womens-clothing-plus-size.html',
+  'http://www.wsdear.com/womens-clothing-blazers.html',
 ]
 
 # $limit = ENV['LIMIT'] || 50
